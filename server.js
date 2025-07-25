8e104f27574a67e312a4e97b97e7c3e5d559459d
spokeraaa <spartacossbc@gmail.com>
2025-07-24 21:41:32 -0300

    Ajusta server.js para usar porta dinâmica para o deploy

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.post('/generate', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
